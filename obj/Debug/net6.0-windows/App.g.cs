71f950be48a1aece294c9b1bb453af414958c2d6
valerio-sandri <138996130+valerio-sandri@users.noreply.github.com>
2023-07-10 13:22:36 +0200

    rebuild

--- obj/Debug/net6.0-windows/App.g.cs
+++ obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace SimpleMVVMApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace SimpleMVVMApp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public static void Main() {
             SimpleMVVMApp.App app = new SimpleMVVMApp.App();
             app.InitializeComponent();
